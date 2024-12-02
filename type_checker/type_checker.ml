d9880186d4ef108e2ab57808c742c8c77ac2589b
suman chapai <sumanchapai@gmail.com>
2024-12-02 13:36:43 -0500

    Revert "TODO: add framework for type checker"
    This reverts commit e4c3c0790b852c2a9507fff5664429cb54dab9c8.

--- type_checker/type_checker.ml
+++
@@ -1,8 +0,0 @@
-(* type_checker.ml *)
-open Ppxlib
-
-(* A dummy function to check types; currently incomplete *)
-let check_type _program =
-  print_endline "Type checking not fully implemented.";
-  true (* Always returns true for now *)
-
