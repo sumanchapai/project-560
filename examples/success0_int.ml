0467d802cebf62de85d50c39ba7498e0b6df4c74
asha soroushpoor <ashasoroushpoor@pal-nat186-2-133.itap.purdue.edu>
2024-12-05 21:14:32 -0500

    fixed demos

--- examples/success0_int.ml
+++ examples/success0_int.ml
@@ -1,3 +1,3 @@
 
 
-let x : int [@refinement v, v  <= 2] = 1 + 1
+let x : int [@refinement v, v  <= 2] = 0
--- examples/success3_int.ml
+++ examples/success3_int.ml
@@ -1,3 +1,2 @@
-let a : int = 1 * (5 - 3)
-let b : int = 1 * (3 - 5)
-let c : int [@refinement v, v < a && v < b]= a * b
+let x: bool = true
+let a: int [@refinement (v, x)] = 5
